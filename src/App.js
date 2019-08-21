af0db145690fa9561c7b2ab73ed693a55a3f951a
Miles Lederman <miles@Miless-Air.hsd1.wa.comcast.net>
2019-08-20 23:59:34 -0700

    finally gaining traction

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import './App.css';
 class App extends React.Component {
 
   state = {
-    habits: [] //This array will be used to keep track of each of the habits
+    habits: [<Habit habitName="pooping my pants"/>, <Habit habitName="peeing my pants"/> ], //This array will be used to keep track of each of the habits
+    colorZero: "#000000",
+    colorOne: "#00000",
+    colorTwo: "#00000",
+    colorThree: "#00000",
   }
 
   render() {
-    var {habits} = this.state
+    var { habits } = this.state
     return (
       <div className="app">
         <h1>Habit Tracker</h1>
         <p>A great way to track your personal progress!</p>
+        {habits.map(habit => {
+          return(
+          <span>
+            {habit}
+            <br/>
+          </span>
+        )})}
       </div>
     );
   }
@@ -21,13 +32,21 @@ class App extends React.Component {
 export default App;
 
 // This is an object for each box that contains the day and the color of the box.
-function Box(props) {
-  var {color, dayNumber} = props
-  return(color, dayNumber)
+function Square(props) {
+  var { color, dayNumber, habitName } = props
+  const tag = habitName + " " + dayNumber
+  console.log(tag)
+  return (<button className = "Square" style={{ backgroundColor: "#000000" }}>
+    {dayNumber}
+  </button>)
 }
 
 // This is an object that contains the boxes for each day of the week, with the name of the habit.
 function Habit(props) {
-  var boxes = {props} //You're going to run a map of boxes as the props
-  var habitName
+  var { habitName } = props
+  var boxes = []
+  for (let j = 1; j < 8; j++) {
+    boxes.push(<Square dayNumber={j} habitName={habitName} />)
+  }
+  return (boxes)
 }
